37bd1cd4f17f11a3211188c5c94040a9ea9a7538
aswinipk <aswinikannan96@gmail.com>
2021-08-03 14:25:11 +0530

    next program

---
+++ Program8.kt
@@ -0,0 +1,19 @@
+// Create a program to print the first 10 numbers of Fibonacci series. The Fibonacci Sequence is a series of numbers: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ... The next number is found by adding up the two numbers before it. The 2 is found by adding the two numbers before it (1+1). The 3 is found by adding the two numbers before it (1+2) and so on! Example: the next number in the sequence above is 21+34 = 55.
+
+
+fun main() {
+    
+    // write your code here
+    var a = 0
+    var b = 1
+    var c = 0
+    print("$a,$b")
+    for (i in 1..8){
+    
+    c = a+b
+    print(",$c")
+    a = b
+    b = c
+    
+}    
+}
