c0323f1120fad4597a2232538c7c47dba18a5c0d
aswinipk <aswinikannan96@gmail.com>
2021-08-03 11:20:36 +0530

    second program

---
+++ program1.kt
@@ -0,0 +1,19 @@
+fun main() {
+    // write your code here
+    //for(i in 1 until 50){
+    //    println(i)
+    //}
+    
+    //var i = 1
+    //while(i<=50){
+    //    println(i)
+    //    i++
+
+    //}
+    
+    var i =1
+    do{
+        println(i)
+        i++
+    }while(i<=50)
+}
