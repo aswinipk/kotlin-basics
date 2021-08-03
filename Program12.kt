295db9835d02cbad45f546fa028ad740ffcc6eec
aswinipk <aswinikannan96@gmail.com>
2021-08-03 21:34:52 +0530

    next program

---
+++ Program12.kt
@@ -0,0 +1,8 @@
+//Initialise a variable x = null and print the value as 0.0 while printing x using the Elvis operator.
+
+fun main(){
+    
+    // write your code here
+   val x: Int? = null
+   println(x?: 0.0)
+}
