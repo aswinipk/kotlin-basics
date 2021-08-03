5ccf678023a5caaf667f5dc3556188f4fb238bf0
aswinipk <aswinikannan96@gmail.com>
2021-08-03 21:34:34 +0530

    next program

---
+++ Program11.kt
@@ -0,0 +1,11 @@
+//Initialise a String as “Hello, Kotlin” and change its value to null. Now print the length of the String using safe call and non-null assertion operator.
+
+fun main(){
+    
+    // write your code here
+    var myString: String? = "Hello, Kotlin"
+    myString = null
+    println(myString?.length)
+    println(myString!!.length)
+   
+}
