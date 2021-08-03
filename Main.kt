491271b250afdc8b138fbdb11fdb7a99dcae339f
aswinipk <aswinikannan96@gmail.com>
2021-08-03 10:58:27 +0530

    first program

---
+++ Main.kt
@@ -0,0 +1,12 @@
+fun main() {
+   // write your code here
+   val a = "Internshala"
+   val b = "Trainings"
+
+if (a == b){
+   println("Length is same")
+} 
+else {
+   println("Length is different")
+}
+}
