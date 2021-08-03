daeabfc570cc45ec2d711a636425becc7c11529e
aswinipk <aswinikannan96@gmail.com>
2021-08-03 14:23:47 +0530

    second program

---
+++ Program7.kt
@@ -0,0 +1,12 @@
+//Create a program in Kotlin to calculate the factorial of number 7.
+
+fun main() {
+    
+    // write your code here
+    var fact = 1
+    for (i in 7 downTo 1){
+        fact *= i
+    }
+    println(fact)
+    
+}
