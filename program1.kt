127700b01548beaa531e0083dd62f2fde1f4bbce
Aswini_pk <33228343+aswinipk@users.noreply.github.com>
2021-08-04 12:24:21 +0530

    Looping

--- program1.kt
+++ program1.kt
@@ -1,8 +1,8 @@
 fun main() {
     // write your code here
-    //for(i in 1 until 50){
-    //    println(i)
-    //}
+    for(i in 1 until 50){
+        println(i)
+    }
     
     //var i = 1
     //while(i<=50){
