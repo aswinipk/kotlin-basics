06baa294a140a2ff68a46127ebef80226f86eaeb
Aswini_pk <33228343+aswinipk@users.noreply.github.com>
2021-08-04 12:34:07 +0530

    Printing odd numbers from 100 to 1

--- Program4.kt
+++ Program4.kt
@@ -1,17 +1,19 @@
 //Print the odd numbers from 100 to 1 using ‘for’, ‘while’, and ‘do-while’ loops.
 
 fun main() {
+   //for loop
 for (i in 99 downTo 1 step 2) {
    println(i)
 }
 
-var j = 99
+//while Loop
+   var j = 99
 while (j >= 1) {
    println(j)
    j -= 2
 }
 
-
+//doWhile Loop
 var k = 99
 do {
    println(k)
