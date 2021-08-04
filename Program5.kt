2ddeb58376ed9528fe0ca5c5a3cff310a72909f9
Aswini_pk <33228343+aswinipk@users.noreply.github.com>
2021-08-04 12:36:09 +0530

    Different types of Lists
    Mutable lists , Immutable Lists and Array List

--- Program5.kt
+++ Program5.kt
@@ -3,12 +3,15 @@
 fun main() {
     
     // write your code here
-   val myImmutableList = listOf("Lionel Messi", "Wayne Rooney", "Luka Modric", "Eden Hazard", "Sergio Aguero")
+    //Immutable List
+val myImmutableList = listOf("Lionel Messi", "Wayne Rooney", "Luka Modric", "Eden Hazard", "Sergio Aguero")
 println(myImmutableList)
 
+    //Utable List
 val myMutableList = mutableListOf("Lionel Messi", "Wayne Rooney", "Luka Modric", "Eden Hazard", "Sergio Aguero")
 println(myMutableList)
-
+    
+    //Array List
 val myArrayList = arrayListOf("Lionel Messi", "Wayne Rooney", "Luka Modric", "Eden Hazard", "Sergio Aguero")
 println(myArrayList)
 }
